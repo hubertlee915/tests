5e2ec38d44263d02ec4239230ba49d4105b4f42d
ydu <ydu14@ur.rochester.edu>
2018-09-25 15:58:33 -0400

    Fixed bad tests for Ryan Spurr's file

--- alltest/ryan_spurr.rb
+++ alltest/ryan_spurr.rb
@@ -1,9 +1,5 @@
 require 'minitest/autorun'
-require './cs253_enum/cs253Enum.rb'
-
-class Array
-    include CS253Enumerable
-end
+require './cs253Array.rb'
 
 class Range
     include CS253Enumerable
@@ -11,25 +7,28 @@ end
 
 class CS253EnumTests < Minitest::Test
     def test_cs253collect
-        int_triple = Array.new([1, 2, 3])
-        str_triple = Array.new(["string", "anotherString", "lastString"])
+        int_triple = CS253Array.new([1, 2, 3])
+        str_triple = CS253Array.new(["string", "anotherString", "lastString"])
 
         assert_equal int_triple.cs253collect{|i| i.to_s}.to_a,["1","2","3"]
         assert_equal str_triple.cs253collect{|i| i.length}.to_a,[6,13,10]
     end
 
     def test_cs253all?
-        assert %w[ant bear cat].cs253all? { |word| word.length >= 3 } == true
-        assert %w[ant bear cat].cs253all? { |word| word.length >= 4 } == false
-        assert [nil, true, 99].cs253all?                              == false
-        assert [].cs253all?
+        str_triple = CS253Array.new(["ant", "bear", "cat"])
+
+        assert str_triple.cs253all? { |word| word.length >= 3 } == true
+        assert str_triple.cs253all? { |word| word.length >= 4 } == false
+        assert CS253Array.new([nil, true, 99]).cs253all? == false
+        assert CS253Array.new([]).cs253all?
     end
 
     def test_cs253any?
-        assert %w[ant bear cat].any? { |word| word.length >= 3 }
-        assert %w[ant bear cat].any? { |word| word.length >= 4 }
-        assert [nil, true, 99].any?
-        assert ![].any?
+	str_triple = CS253Array.new(["ant", "bear", "cat"])
+        assert str_triple.any? { |word| word.length >= 3 }
+        assert str_triple.any? { |word| word.length >= 4 }
+        assert CS253Array.new([nil, true, 99]).any?
+        assert !CS253Array.new([]).any?
     end
 
 
